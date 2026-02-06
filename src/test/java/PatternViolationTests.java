09cb40a177b3a173fa5ec7dbd25c89afa3212532
rameshr <rameshr@rose-hulman.edu>
2026-02-06 13:46:24 -0500

    test: add PatternViolationTests skeleton

---
+++ src/test/java/PatternViolationTests.java
@@ -0,0 +1,16 @@
+import Domain.LintResult;
+import Domain.PrincipleCheck.EncapsulationCheck;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class PatternViolationTests {
+}
