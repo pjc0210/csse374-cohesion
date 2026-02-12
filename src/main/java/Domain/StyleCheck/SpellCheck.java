845afda0a4f72a357d8229f0de9a759683798308
SandyaSuresh <sandya.suresh@gmail.com>
2026-02-11 23:31:13 -0500

    using languagetool, but issues with java,util lib

--- src/main/java/Domain/StyleCheck/SpellCheck.java
+++ src/main/java/Domain/StyleCheck/SpellCheck.java
@@ -1,45 +1,103 @@
-//package Domain.StyleCheck;
+package Domain.StyleCheck;
+
+import Domain.Category;
+import Domain.Interfaces.IStyleCheck;
+import Domain.LintResult;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.AmericanEnglish;
+import org.objectweb.asm.tree.*;
+
+import java.util.*;
+
+
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A style check that verifies spelling in the code.
+ */
+
+public class SpellCheck implements IStyleCheck {
+    JLanguageTool langTool = new JLanguageTool(new AmericanEnglish());
+
+    @Override
+    public List<LintResult> execute(ClassNode classNode) {
+        List<LintResult> lintResults = new ArrayList<>();
 //
-//import Domain.Interfaces.IStyleCheck;
-//import Domain.LintResult;
-//import org.objectweb.asm.tree.ClassNode;
+//        try {
+//            lintResults.addAll(checkWord(classNode.name, "class name"));
 //
-//import java.util.ArrayList;
-//import java.util.List;
+//            for (FieldNode field : classNode.fields) {
+//                lintResults.addAll(checkWord(field.name, "field name"));
+//            }
+//            for (MethodNode method : classNode.methods) {
+//                lintResults.addAll(checkWord(method.name, "method name"));
+//                for (AbstractInsnNode insn : method.instructions) {
+//                    if (insn instanceof FieldInsnNode) {
+//                        lintResults.addAll(checkWord(((FieldInsnNode) insn).name, insn));;
+//                    }
+//                }
+//                for (ParameterNode p : method.parameters) {
+//                    lintResults.addAll(checkWord(p.name, "parameter name in method" + method.name));
+//                }
+//                for(LocalVariableNode var: method.localVariables){
+//                    lintResults.addAll(checkWord(var.name, "local variable name in method" + method.name));
+//                }
+//            }
 //
-//import com.jspell.domain.*;
-//import com.jspell.gui.*;
-//
-//import javax.swing.*;
-//
-///**
-// * A style check that verifies spelling in the code.
-// */
-//
-//public class SpellCheck implements IStyleCheck {
-//    JSpellDictionaryLocal jdLocal;
-//    JSpellParser parser;
-//    JSpellChecker dialog;
-//    JSpellDictionaryManager manager;
-//    JTextArea text = new JTextArea();
-//
-//    @Override
-//    public List<LintResult> execute(ClassNode classNode) {
-//        List<LintResult> lintResults = new ArrayList<>();
-//
-//        manager = JSpellDictionaryManager.getJSpellDictionaryManager();
-//        manager.setDictionaryDirectory("./lexicons/");
-//        parser = new JSpellParser(manager.getJSpellDictionaryLocal("English (US)"));
-//        dialog = new JSpellChecker();
-//        dialog.setParser(parser);
-//        dialog.addComponent(text);
-//        dialog.check();
-//
-//        return lintResults;
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return "Spelling";
-//    }
-//}
+//        }   catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        return lintResults;
+    }
+
+    protected List<LintResult> checkWord(String str, AbstractInsnNode insn) throws IOException {
+        List<String> indWords = getWords(str);
+        List<LintResult> lintResults = new ArrayList<>();
+//        for(String word : indWords){
+//            List<RuleMatch> matches = langTool.check(word);
+//            if (!matches.isEmpty()) {
+//                String message = "✗ '" + word + "' has spelling errors:";
+//                lintResults.add(new LintResult(getName(), Category.STYLE, "On line " + getLineNumber(insn), message));
+//            }
+//        }
+        return lintResults;
+    }
+
+    protected List<LintResult> checkWord(String str, String type) throws IOException {
+        List<String> indWords = getWords(str);
+        List<LintResult> lintResults = new ArrayList<>();
+//        for(String word : indWords){
+//            List<RuleMatch> matches = langTool.check(word);
+//            if (!matches.isEmpty()) {
+//                String message = "✗ '" + word + "' has spelling errors:";
+//                lintResults.add(new LintResult(getName(), Category.STYLE, "Error in " + type, message));
+//            }
+//        }
+        return lintResults;
+    }
+
+    private int getLineNumber(AbstractInsnNode insn) {
+        while (insn != null) {
+            if (insn instanceof LineNumberNode) {
+                return ((LineNumberNode) insn).line;
+            }
+            insn = insn.getPrevious();
+        }
+        return -1;
+    }
+    protected List<String> getWords(String str) {
+        String[] parts = str.split("(?=\\p{Upper})");
+//        for (String part : parts) {
+//            System.out.println(part);
+//        }
+        return Arrays.asList(parts);
+    }
+
+    @Override
+    public String getName() {
+        return "Spelling";
+    }
+}
