077c0f393008dbf257d53a34a5a8f6d071a30562
PJ <75004958+pjc0210@users.noreply.github.com>
2026-02-20 13:44:48 -0500

    minor changes

--- src/test/java/UnusedParametersCheckTests.java
+++ src/test/java/UnusedParametersCheckTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UnusedParametersCheckTest {
+class UnusedParametersCheckTests {
 
     private UnusedParametersCheck check;
 
